a8c2437dc77075191f5912fc61bf619efdfa5f65
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-06-25 17:33:02 +0600

    wroking on add story btn

--- components/feed/story/AddStoryCard.jsx
+++ components/feed/story/AddStoryCard.jsx
@@ -14,8 +14,8 @@ export default function AddStoryCard() {
         <Image src={image} alt="profile"
           className="h-[8rem] w-40 rounded-t-lg object-cover" width={40} height={40} />
       </div>
-      <div className="h-[8rem] w-40 rounded-b-lg object-cover bg-[#323436]">
-          <h2 className="text-white text-[1.2rem] font-bold">{name}</h2>
+      <div className="h-[8rem] w-40 rounded-b-lg text-center m-auto bg-[#323436]">
+          <h2 className="text-white text-[1.2rem] font-bold align-middle mt-20">{name}</h2>
       </div>
     </div>
   )
