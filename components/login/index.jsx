a8c7bb264129b356ae8f97fb2395ce029d938a81
rayhanalshorif133 <rayhanalshorif@gmail.com>
2023-06-27 13:26:06 +0600

    sign in page is now working

--- components/login/index.jsx
+++ components/login/index.jsx
@@ -4,19 +4,19 @@ import Form from './Form'
 
 export default function Index() {
   return (
-    <div className='h-screen bg-gray-100 overflow-hidden'>
+    <div className='h-screen bg-gray-100 overflow-scroll'>
       <Head>
         <link rel="icon" href="/assets/icons/facebook_logo_icon.ico" sizes="any" />
         <title>Facebook - log in or sign up</title>
       </Head>
       <main className='bg-[#F0F2F5]'>
-        <div className="flex xl:items-center justify-between mx-auto w-[55%]">
-          <div className="">
+        <div className="flex xl:items-center justify-between mx-auto w-[55%] xl:w-[75%]">
+          <div className="w-2/3">
             <h2 className="text-[#1877F2] text-[4rem] font-bold">facebook</h2>
-            <p className="text-black text-[1.7rem] font-[480]">
+            <p className="text-black text-[1.7rem] font-[480] xl:font-[490] leading-none">
               Facebook helps you connect and share <br /> with the people in your life.</p>
           </div>
-          <div className="">
+          <div className="xl:mt-24 2xl:mt-24">
             <Form />
           </div>
         </div>
