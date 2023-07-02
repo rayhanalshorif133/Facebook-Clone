3660dcc1f2b63124e9c7c4a818a0295fdf5cce7e
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-07-02 09:59:03 +0600

    adjust header in leag screen

--- components/Header/Index.jsx
+++ components/Header/Index.jsx
@@ -69,7 +69,7 @@ export default function Index() {
 
           </div>
         </div>
-        <div className='flex justify-center sm:mx-[3rem] xl:ml-[6rem] xl:mr-[26.8rem]'>
+        <div className='flex justify-center sm:mx-[3rem] xl:ml-[6rem] 2xl:mx-auto xl:mr-[26.8rem]'>
           <div className='flex space-x-6 xl:space-x-1 md:space-x-2'>
             <HeaderMiddleIcons Icon={FaHome} active={true} />
             <HeaderMiddleIcons Icon={FiUsers} />
