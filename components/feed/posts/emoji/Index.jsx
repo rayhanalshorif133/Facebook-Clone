123ebd251f25609cc2f06ea59a0037cfd09b39d2
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-07-06 14:13:10 +0600

    doc added

--- components/feed/posts/emoji/Index.jsx
+++ components/feed/posts/emoji/Index.jsx
@@ -1,32 +1,48 @@
-import React, { useContext } from 'react'
+import React, { useContext, createContext } from 'react'
 import { emojiContext } from '../Post';
 import SingleItem from './SingleItem';
 
+export const tooltipContext = createContext({});
+
 export default function Index() {
 
-  const { setShowEmoji,handleRemoveReactEmoji } = useContext(emojiContext);
+    const { setShowEmoji, handleRemoveReactEmoji } = useContext(emojiContext);
+    const [tooltip, setTooltip] = React.useState('');
+    const [marginLeft, setMarginLeft] = React.useState(3);
 
-  const handleHoverEmoji = () => {
-    setShowEmoji(true);
-    handleRemoveReactEmoji(true);
-  } 
-  const handleOnLeave = () => {
-    setShowEmoji(true);
-    handleRemoveReactEmoji(false);
-  }
-  
+    const marginLeftValue = 3;
 
-  return (
-    <div className='absolute bg-gray-800 rounded-2xl w-[23.3rem] h-10 text-white'>
-      <div className='flex justify-around items-center' onMouseEnter={handleHoverEmoji} onMouseLeave={handleOnLeave}>
-        <SingleItem title='like' image='assets/emoji/fb-emoji/like.png' />
-        <SingleItem title='love' image='assets/emoji/fb-emoji/love.png' />
-        <SingleItem title='care' image='assets/emoji/fb-emoji/care.png' />
-        <SingleItem title='haha' image='assets/emoji/fb-emoji/haha.png' />
-        <SingleItem title='wow' image='assets/emoji/fb-emoji/wow.png' />
-        <SingleItem title='sad' image='assets/emoji/fb-emoji/sad.png' />
-        <SingleItem title='angry' image='assets/emoji/fb-emoji/angry.png' />
-      </div>
-    </div>
-  )
+    const handleHoverEmoji = () => {
+        setShowEmoji(true);
+        handleRemoveReactEmoji(true);
+    }
+    const handleOnLeave = () => {
+        setShowEmoji(true);
+        handleRemoveReactEmoji(false);
+    }
+
+    const handleTooltip = (e) => {
+        console.log(e.target.dataset.ml);
+        setMarginLeft(e.target.dataset.ml);
+        setTooltip(e.target.alt);
+    }
+
+
+    return (
+        <div className=''>
+            <div className='absolute bg-transparent  w-[23.3rem] h-20 text-white bottom-0 mt-10'>
+                <div className='flex justify-around items-center bg-gray-800 rounded-2xl' onMouseEnter={handleHoverEmoji} onMouseLeave={handleOnLeave}>
+                    <tooltipContext.Provider value={{handleTooltip}}>
+                        <SingleItem title='like' image='assets/emoji/fb-emoji/like.png' />
+                        <SingleItem title='love' image='assets/emoji/fb-emoji/love.png' ml="7rem"/>
+                        <SingleItem title='care' image='assets/emoji/fb-emoji/care.png' ml="10rem"/>
+                        <SingleItem title='haha' image='assets/emoji/fb-emoji/haha.png' ml="10.2rem"/>
+                        <SingleItem title='wow' image='assets/emoji/fb-emoji/wow.png' ml="10rem"/>
+                        <SingleItem title='sad' image='assets/emoji/fb-emoji/sad.png' ml="10rem"/>
+                        <SingleItem title='angry' image='assets/emoji/fb-emoji/angry.png' ml="10rem"/>
+                    </tooltipContext.Provider>
+                </div>
+            </div>
+        </div>
+    )
 }
--- components/feed/posts/emoji/SingleItem.jsx
+++ components/feed/posts/emoji/SingleItem.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
-export default function SingleItem({title,image}) {
+export default function SingleItem({ title, image }) {
+
+
+    const [tooltip, setTooltip] = React.useState(false);
+
+
+    const handleTooltip = (e) => {
+        setTooltip(true);
+    }
+
+    const handleTooltipLeave = (e) => {
+        setTooltip(false);
+    }
+
+    {/*
+    {
+        tooltip === true && <h3 className="text-white bg-black justify-center items-center m-auto rounded-lg text-center mb-2">{title}</h3>
+    }  
+*/}
+
     return (
-        <>
-            <img className='w-10 h-10 cursor-pointer hover:scale-110 transition-all ease-in-out' src={image} alt={title} />
-        </>
+        <div onMouseEnter={handleTooltip} onMouseLeave={handleTooltipLeave}>
+            {
+                tooltip === true && <h3 className="text-white absolute z-10 bg-black justify-center items-center m-auto rounded-lg text-center top-0 px-2">{title}</h3>
+            }
+            <img className='w-10 h-10 cursor-pointer hover:scale-110 mt-8' src={image} alt={title} />
+        </div>
     )
 }
