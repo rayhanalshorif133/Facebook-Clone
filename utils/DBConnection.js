4747eee2a0d74e89620a915c5734b4d0c706d005
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-07-07 23:06:07 +0600

    change mongodb atlas

--- utils/DBConnection.js
+++ utils/DBConnection.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose';
 
 // Connect to MongoDB
 const DBConnection = async () => {
-   await mongoose.connect('mongodb://127.0.0.1:27017/facebook');
+    // const URL = prosess.env.NEXT_PUBLIC_MONGO_URI || 'mongodb://127.0.0.1:27017/facebook';
+    const URL = 'mongodb+srv://rayhanalshoriffacebookclone:rayhanalshoriffacebookclone@cluster0.xviapgc.mongodb.net/facebook?retryWrites=true&w=majority' || 'mongodb://127.0.0.1:27017/facebook';
+   await mongoose.connect(URL);
     console.log('Database connection established ...!!!');
 };
 
