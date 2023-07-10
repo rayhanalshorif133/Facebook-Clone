ca0b0faedeaf3a3b186e9b6850d972fa12bbe543
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-07-10 14:56:39 +0600

    added auth next

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import LoginPage  from '@/components/auth/login/Index';
+import { useSession } from 'next-auth/react'
+import Dashboard from '@/components/home/Dashboard';
 
 export default function index() {
-  return (
-    <LoginPage />
-  )
+  const { data: session } = useSession();
+  if (session) {
+    return (<Dashboard />)
+  }else{
+    return (<LoginPage />)
+  }
 }
+
 
