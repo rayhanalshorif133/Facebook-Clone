10d73007966b02c7db1452918d2cd0ce10886d38
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-07-10 14:59:48 +0600

    signout btn ready

--- components/home/Dashboard.js
+++ components/home/Dashboard.js
@@ -1,7 +1,15 @@
+import { signOut } from 'next-auth/react'
 import React from 'react'
 
 export default function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+    return (
+        <div>
+            Dashboard
+            <div className='flex justify-between mx-auto items-center w-11/12 mt-16'>
+                <button onClick={() => signOut()} className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full'>
+                    Sign Out
+                </button>
+            </div>
+        </div>
+    )
 }
