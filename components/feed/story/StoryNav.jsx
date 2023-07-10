1398d9819d9de846f3fbd4df522c583930d30ba1
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-07-10 12:34:31 +0600

    just index and remove all component

--- components/feed/story/StoryNav.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function StoryNav({ Icon, title, isActiveStatus = false, clickAbleToggle }) {
-    return (
-        <div onClick={clickAbleToggle} className={`flex items-center justify-center mx-auto cursor-pointer 2xl:w-full 2xl:mt-2 sm:w-20 m-5 ${isActiveStatus? 'border-b-3 border-blue-500 text-blue-500 2xl:mt-5' : 'text-gray-400 hover:bg-[#3A3B3C] pt-4 rounded-xl'} `}>
-            <Icon className="h-6 w-6 ml-1 mb-4" />
-            <p className="font-bold ml-1 mb-4">{title}</p>
-        </div>
-    )
-}
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,8 @@
-import Header from '@/components/Header/Index'
-import { getSession, useSession } from 'next-auth/react'
-import Head from 'next/head'
+import React from 'react'
 
-
-export default function Home({session}) {
-
-
+export default function index() {
   return (
-    <div className='h-screen bg-[#18191A] overflow-x-hidden'>
-      <Head>
-        <link rel="icon" href="/assets/icons/facebook_logo_icon.ico" sizes="any" />
-        <title>Facebook</title>
-      </Head>
-      <Header />
-    </div>
+    <div>index</div>
   )
 }
 
-
-export async function getServerSideProps(context) {
-  const session = await getSession(context);
-  return {
-    props: {
-      session,
-    }// In seconds after a successful request to validate the request
-  }
-}
